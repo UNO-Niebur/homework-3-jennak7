ae10586e1b984e43f4b6360df64f9acbb651beba
jennak7 <136638747+jennak7@users.noreply.github.com>
2026-04-02 12:59:45 +0000

    progress

--- BoardGame.py
+++ BoardGame.py
@@ -1,6 +1,6 @@
 # Homework 3 - Board Game System
-# Name:
-# Date:
+# Name:Jenna Kramer
+# Date:04/01/26
 
 def loadGameData(filename):
     """Reads game data from a file and returns it as a list."""
@@ -20,9 +20,22 @@ def displayGame(data):
 
 def movePlayer(data):
     """Example function to simulate moving a player."""
-    print("\nMove player function not fully implemented.")
+    # print("\nMove player function not fully implemented.")
     # Students will modify this
+    # print(data)
+    playerTurn = data[0].split()
+    for x in range(1, len(data)):
+        item = data[x]
+        if playerTurn[1] in item:
+            splitItem = item.split(": ")
+            position = int(splitItem[0])
+            position += 1 
+            splitItem[0] = str(position)
+            result = ": ".join(splitItem)
+            data[x] = result
 
+    for line in data:
+        print(line)
 
 def main():
     filename = "events.txt"   # Students can rename if needed
